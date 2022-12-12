0e124874127f7814462fdd0976c2da58cc89bb88
MEZMUSIKA <16.meetme@gmail.com>
2022-12-12 13:44:46 -0800

    Combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 {
 int ktr, num;
 
-for (ktr = 0; ktr < 10; ktr++)
+for (ktr = 0; ktr < 9; ktr++)
 {
-for (num = 0; num < 10; num++)
+for (num = 0; num <= 9; num++)
 {
 if (!((ktr == num) || (ktr > num)))
 {
 putchar((ktr % 10) + '0');
 putchar((num % 10) + '0');
 }
-if (!(num == 9 && ktr == 8))
+if ((!(num == 9 && ktr == 8)))
 {
 putchar(',');
 putchar(' ');
