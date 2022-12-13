3376e8965598cf2ca78dd4967e0661b79d927796
MEZMUSIKA <16.meetme@gmail.com>
2022-12-13 04:10:44 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,19 +13,19 @@ int m;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is", n);
+printf("Last digit of % d is", n);
 m = n % 10;
 if (m > 5)
 {
-printf("%d and is greater than 5\n", m);
+printf("% d and is greater than 5\n", m);
 }
 else if (m == 0)
 {
-printf("%d and is 0\n", m);
+printf("% d and is 0\n", m);
 }
 else if ((m < 6) && (m > 0) && (m < 0))
 {
-printf("%d and is less than 6 and not 0\n", m);
+printf("% d and is less than 6 and not 0\n", m);
 }
 return (0);
 }
