44d0c59b67689cb3152e83462ddc6d4b235cbb7c
MEZMUSIKA <16.meetme@gmail.com>
2022-12-13 15:01:23 -0800

    Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print the lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+int f = 'a';
+int k = '0';
+
+while (k < 10)
+{
+for (f <= 'z')
+_putchar(f);
+f++;
+}
+_putchat('\n');
+}
