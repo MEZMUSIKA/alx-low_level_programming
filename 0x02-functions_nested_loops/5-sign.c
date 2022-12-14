eca9d2f6a68f08a54bb960fd734bd7addd898624
MEZMUSIKA <16.meetme@gmail.com>
2022-12-14 07:40:19 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,24 +7,19 @@
  *
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
- * -1 and print - if n is less than zero.
+ * (-1) and print - if n is less than zero.
  */
 int print_sign(int n)
 {
 if (n > 0)
-{
 _putchar('+');
 return (1);
-
 if (n == 0)
-
 _putchar('0');
 return (0);
-}
-else
-{
+else 
 _putchar('-');
-return ((-1));
+return ('-1');
 }
 
 
