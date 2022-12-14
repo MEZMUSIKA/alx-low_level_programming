e91ddb53ccba957245e5ac4a5d6aff962061092a
MEZMUSIKA <16.meetme@gmail.com>
2022-12-14 10:21:17 -0800

    Nine times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - print 9x table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+int dig, mul, res;
+
+for (dig = 0; dig <= 9; dig++)
+{
+_putchar('0');
+for (mul = 0; mul <= 9; mul++)
+{
+_putchar(',');
+_putchar(' ');
+res = dig * mul;
+if (res <= 9)
+_putchar(' ');
+else
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+_putchar('\n');
+}
