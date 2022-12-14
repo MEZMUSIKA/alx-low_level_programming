82d6b25a0ae9e529c16c66e2c9b37bc04461d670
MEZMUSIKA <16.meetme@gmail.com>
2022-12-14 08:41:37 -0800

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - Function that prints the last digit of a number
+ *
+ * @k: number digits
+ *
+ * Return: 0
+ */
+int print_last_digit(int k)
+{
+int ln = k % 10;
+
+if (ln < 0)
+ln *= -1;
+_putchar(ln + '0');
+return (0);
+}
