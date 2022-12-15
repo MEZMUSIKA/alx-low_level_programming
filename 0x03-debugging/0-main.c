56d0ca63675884bf0865c683c9b0b8e134a02fb8
MEZMUSIKA <16.meetme@gmail.com>
2022-12-15 11:01:44 -0800

    Debugging 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,31 @@
-#include "main.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
-}                                                       
-                                                                                         
+#include <stdlib.h>                                                                        
+#include <stdio.h>                                                                         
+#include <time.h>                                                                          
+                                                                                           
+/**                                                                                        
+ * main - Entry point                                                                      
+ *                                                                                         
+ * Return: Always 0 (Success)                                                              
+ */                                                                                        
+                                                                                           
+int main(void)                                                                             
+{                                                                                          
+int n;                                                                                     
+                                                                                           
+srand(time(0));                                                                            
+n = rand() - RAND_MAX / 2;                                                                 
+if (n > 0)                                                                                 
+{                                                                                          
+printf("%d is positive\n", n);                                                             
+}                                                                                          
+else if (n == 0)                                                                           
+{                                                                                          
+printf("%d is zero\n", n);                                                                 
+}                                                                                          
+else if (n < 0)                                                                            
+{                                                                                          
+printf("%d is negative\n", n);                                                             
+}                                                                                          
+return (0);                                                                                
+}                                                                            
 
