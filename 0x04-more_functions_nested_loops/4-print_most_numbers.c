e0bfb6eca4ba4320f34faa57b269c22c5bda0104
MEZMUSIKA <16.meetme@gmail.com>
2022-12-16 14:12:56 -0800

    Print Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,17 @@
 
 /**
  * print_most_numbers -  print numbers except 2 and 4
- *
  * Return: 0
  */
 
 void print_most_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		if (!(i == '2' || i == '4'))
-			_putchar(i);
-	}
-	_putchar('\n');
+for (i = '0'; i <= '9'; i++)
+{
+if (!(i == '2' || i == '4'))
+_putchar(i);
 }
-
+_putchar('\n');
+}
