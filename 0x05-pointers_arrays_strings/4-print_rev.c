b25ebd23ad08950f7e084709438d13382aa85d66
MEZMUSIKA <16.meetme@gmail.com>
2022-12-20 14:41:57 -0800

    String Revers function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_rev - Write a function that prints a string, in reverse
+ * @s: the string to be printed 
+ *
+ */
+
+void print_rev(char *s)
+{
+	while (*str)
+	{
+		_putchar(
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * rev_string - write a function that revers a string
+ * @s: input of the function
+ *
+ */
+
+void rev_string(char *s)
+{
+	while (*s)
+
+	_putchar ("strrev");
+
+}
+
