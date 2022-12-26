1be3a8e88b3253ea94d0a3fb9b9f80b245395066
MEZMUSIKA <16.meetme@gmail.com>
2022-12-26 09:16:30 -0800

    Print string in revers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,4 @@
+#includee "main.h"
+
+/**
+ *
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev - Write a function that prints a string, in reverse
- * @s: the string to be printed 
+ * @s: the string to be printed
  *
  */
 
 void print_rev(char *s)
 {
-	while (*str)
-	{
-		_putchar(
+int k, l, m;
+
+k = 0;
+
+while (s[k] != '\0')
+{
+	k++;
+}
+m = k;
+for (l = m - 1; l >= 0; l--)
+{
+	_putchar(s[l]);
+}
+-putchar('\n');
+}
